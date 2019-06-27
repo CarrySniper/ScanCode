b163ace395d4c0aa75ba83ab1bba26b60f8ca832
cjq <cjq.yingju.com>
2019-06-27 14:39:26 +0800

    优化生成二维码性能

---
+++ OperateHeroApp/OperateHeroApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OperateHeroApp
+//
+//  Created by CL on 2019/6/13.
+//  Copyright © 2019 OperateHeroApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OperateHeroApp/OperateHeroApp/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  OperateHeroApp
+//
+//  Created by CL on 2019/6/13.
+//  Copyright © 2019 OperateHeroApp. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+
+@end
